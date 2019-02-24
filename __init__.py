2cb36e1c814d1644fb0b009e63374b54c87905f3
builderjer <builderjer@gmail.com>
2019-02-24 09:14:03 -0700

    uses mycroft language instead of home.mycroft.ai setting

--- __init__.py
+++ __init__.py
@@ -36,7 +36,8 @@ class Tmdb(MycroftSkill):
 	
 	def initialize(self):
 		TMDB["tmdb"].api_key = self.settings.get("apiv3")
-		TMDB["tmdb"].language = self.settings.get("language")
+		TMDB["tmdb".language = self.lang
+		#TMDB["tmdb"].language = self.settings.get("language")
 		
 ##################
 # Movie Section
