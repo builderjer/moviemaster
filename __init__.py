02cc47f3d773df26448da9b3dd8d0ba5cd4ae277
builderjer <builderjer@gmail.com>
2019-02-24 20:08:58 -0700

    Added search by genre

--- __init__.py
+++ __init__.py
@@ -14,8 +14,8 @@ TMDB = {
 		#"collection": tmdbv3api.Collection(),
 		#"company": tmdbv3api.Company(),
 		#"configuration": tmdbv3api.Configuration(),
-		#"discover": tmdbv3api.Discover(),
-		#"genre": tmdbv3api.Genre(),
+		"discover": tmdbv3api.Discover(),
+		"genre": tmdbv3api.Genre(),
 		"movie": tmdbv3api.Movie()
 		#"person": tmdbv3api.Person(),
 		#"season": tmdbv3api.Season(),
@@ -27,10 +27,18 @@ class Tmdb(MycroftSkill):
 		super(Tmdb, self).__init__(name="Tmdb")
 		self.movieID = None
 		self.movieDetails = None
+		self.movieGenres = self.getGenres()
 
 	def initialize(self):
 		TMDB["tmdb"].api_key = self.settings.get("apiv3")
 		TMDB["tmdb"].language = self.lang
+		
+	def getGenres(self):
+		genres = {
+			"movies": TMDB["genre"].movie_list(),
+			"television": TMDB["genre"].tv_list()
+			}
+		return genres
 
 ##################
 # Movie Section
@@ -55,43 +63,43 @@ class Tmdb(MycroftSkill):
 			except IndexError:
 				self.resetMovieDetails()
 			return False
-		
-	def getDate(self):
+
+	def getMovieDate(self):
 		return nice_date(datetime.strptime(self.movieDetails.release_date.replace("-", " "), "%Y %m %d"))
 
-	def getCast(self):
+	def getMovieCast(self):
 		depth = self.settings.get("cast_depth")
 		return self.movieDetails.casts['cast'][:depth]
 
-	def getBudget(self):
+	def getMovieBudget(self):
 		return pronounce_number(self.movieDetails.budget)
 
-	def getRevenue(self):
+	def getMovieRevenue(self):
 		return pronounce_number(self.movieDetails.revenue)
 
-	def getProductionCo(self):
+	def getMovieProductionCo(self):
 		depth = self.settings.get("pro_co_depth")
 		return self.movieDetails.production_companies[:depth]
 
-	def getRuntime(self):
+	def getMovieRuntime(self):
 		return nice_number(self.movieDetails.runtime)
 
-	def getOverview(self):
+	def getMovieOverview(self):
 		return self.movieDetails.overview
 
-	def getTagline(self):
+	def getMovieTagline(self):
 		return self.movieDetails.tagline
 
-	def getGenres(self):
+	def getMovieGenres(self):
 		depth = self.settings.get("genre_depth")
 		return self.movieDetails.genres[:depth]
-
+		
 	@intent_file_handler("movie.information.intent")
 	def handle_movie_information(self, message):
 		movie = message.data.get("movie")
 		if self.checkMovie(movie):
-			self.speak_dialog("movie.info.response", {"movie": self.movieDetails.title, "year": self.getDate(), "budget": self.getBudget()})
-			self.speak(self.getTagline())
+			self.speak_dialog("movie.info.response", {"movie": self.movieDetails.title, "year": self.getMovieDate(), "budget": self.getMovieBudget()})
+			self.speak(self.getMovieTagline())
 		else:
 			self.speak_dialog("no.info", {"movie": movie})
 
@@ -100,7 +108,7 @@ class Tmdb(MycroftSkill):
 		movie = message.data.get("movie")
 		if self.checkMovie(movie):
 			movie = self.movieDetails.title
-			release_date = self.getDate()
+			release_date = self.getMovieDate()
 			self.speak_dialog("movie.year", {"movie": movie, "year": release_date})
 		else:
 			self.speak_dialog("no.info", {"movie": movie})
@@ -123,7 +131,7 @@ class Tmdb(MycroftSkill):
 	def handle_movie_cast(self, message):
 		movie = message.data.get("movie")
 		if self.checkMovie(movie):
-			cast = self.getCast()
+			cast = self.getMovieCast()
 			actorList = ""
 			last = cast.pop()
 			lastActor = " {} as {}".format(last['name'], last['character'])
@@ -138,7 +146,7 @@ class Tmdb(MycroftSkill):
 	def handle_movie_production(self, message):
 		movie = message.data.get("movie")
 		if self.checkMovie(movie):
-			company = self.getProductionCo()
+			company = self.getMovieProductionCo()
 			noOfCo = len(company)
 			if noOfCo == 1:
 				self.speak_dialog("movie.production.single", {"movie": movie, "company": company[0]["name"]})
@@ -154,7 +162,7 @@ class Tmdb(MycroftSkill):
 	@intent_file_handler("movie.genres.intent")
 	def handle_movie_genre(self, message):
 		if self.checkMovie(movie):
-			genres = self.getGenres()
+			genres = self.getMovieGenres()
 			noOfGenres = len(genres)
 			if noOfGenres == 1:
 				self.speak_dialog("movie.genre.single", {"movie": movie, "genre": genres[0]["name"]})
@@ -171,11 +179,36 @@ class Tmdb(MycroftSkill):
 	def handle_movie_length(self, message):
 		movie = message.data.get("movie")
 		if self.checkMovie(movie):
-			runtime = self.getRuntime()
+			runtime = self.getMovieRuntime()
 			self.speak_dialog("movie.runtime", {"movie": movie, "runtime": runtime})
 		else:
 			self.speak_dialog("no.info", {"movie": movie})
 
+##################
+# Discover Section
+##################
+
+	def getGenreMovies(self, genre):
+		#TODO: Put depth settings in home.ai?
+		depth = self.settings.get("search_depth")
+		genreID = None
+		for g in self.movieGenres["movies"]:
+			if genre == g.name.lower():
+				genreID = g.id
+				return TMDB["discover"].discover_movies({"sort_by": "popularity.desc", "with_genres": genreID})[:depth]
+		return False
+
+	@intent_file_handler("genre.movie.search.intent")
+	def handle_genre_movie_search(self, message):
+		genre = message.data.get("genre")
+		goodGenre = self.getGenreMovies(genre)
+		if goodGenre:
+			self.speak_dialog("genre.movie.search", {"genre": genre})
+			for g in goodGenre:
+				self.speak(g.title)
+		else:
+			self.speak_dialog("bad.genre.catagory", {"genre": genre})
+			
 def create_skill():
 	return Tmdb()
 
